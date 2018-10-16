fd39e4ec7580a87b59a51f6535f7f37f275fdd36
Samuel <samuelchetrit@hotmail.fr>
2018-10-17 01:09:04 +0200

    ijrj

---
+++ morpion.rb
@@ -0,0 +1,237 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+turn = 0
+i = 0
+puts "Quel est le nom du premier joueur"
+print ">"
+name1 = gets.chomp
+
+puts "Quel est le nom du deuxueme joueur"
+print ">"
+name2 = gets.chomp
+
+while (i != 1 || turn != 10)
+
+  puts "choissez un chiffre entre 1 et 9"
+  print">"
+  x = gets.chomp
+  x = x.to_i
+  x = x - 1
+
+  if turn % 2 == 0
+    if board[x] == " "
+      puts name1 + " a joué"
+      board[x] = "X"
+      display_board(board)
+        if board[0] == "X" && board[1] == "X" && board[2] == "X"
+          puts "Vous avez gagnez"
+          i = 1
+          turn = 9 + 1
+          puts "Voulez vous rejou? Y/N"
+          replay= gets.chomp
+          	if replay == "Y"
+              board = [" "," "," "," "," "," "," "," "," "]
+              turn = 0
+              i = 0
+            end
+        end
+        if board[3] == "X" && board[4] == "X" && board[5] == "X"
+          puts "Vous avez gagnez"
+          i = 1
+          puts "Voulez vous rejou? Y/N"
+          replay= gets.chomp
+          	if replay == "Y"
+              board = [" "," "," "," "," "," "," "," "," "]
+              turn = 0
+              i = 0
+            end
+        end
+          if board[6] == "X" && board[7] == "X" && board[8] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[0] == "X" && board[3] == "X" && board[6] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[1] == "X" && board[4] == "X" && board[7] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[2] == "X" && board[5] == "X" && board[8] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[0] == "X" && board[4] == "X" && board[8] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[2] == "X" && board[4] == "X" && board[6] == "X"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+        else
+          puts "mauvaise case"
+          turn = turn - 1
+        end
+  else
+    if board[x] == " "
+        puts name2 + " a joué"
+        board[x] = "O"
+        display_board(board)
+        if board[0] == "O" && board[1] == "O" && board[2] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[3] == "O" && board[4] == "O" && board[5] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[6] == "O" && board[7] == "O" && board[8] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[0] == "O" && board[3] == "O" && board[6] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[1] == "O" && board[4] == "O" && board[7] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[2] == "O" && board[5] == "O" && board[8] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+          end
+          if board[0] == "O" && board[4] == "O" && board[8] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            puts "Voulez vous rejou? Y/N"
+            replay= gets.chomp
+            	if replay == "Y"
+                board = [" "," "," "," "," "," "," "," "," "]
+                turn = 0
+                i = 0
+              end
+
+          end
+          if board[2] == "O" && board[4] == "O" && board[6] == "O"
+            puts "Vous avez gagnez"
+            i = 1
+            if replay == "Y"
+              board = [" "," "," "," "," "," "," "," "," "]
+              turn = 0
+              i = 0
+            end
+          end
+        else
+          puts "mauvaise case"
+          turn = turn - 1
+      end
+end
+turn = turn + 1
+if turn == 9
+  i = 1
+  turn = turn + 1
+  puts "égalite, tout est remplit"
+  puts "Voulez vous rejou? Y/N"
+  replay= gets.chomp
+  	if replay == "Y"
+      board = [" "," "," "," "," "," "," "," "," "]
+      turn = 0
+      i = 0
+    end
+end
+
+end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,14 @@
-
 class BoardCase
   #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide), ainsi que son numéro de case)
+attr_accessor :value, :pos
 
-  
   def initialize
-    #TO DO doit régler sa valeur, ainsi que son numéro de case
+    @value = 0
+    @pos = pos
   end
-  
+
   def to_s
-    #TO DO : doit renvoyer la valeur au format string
+    @value.to_s
   end
 
 end
@@ -16,37 +16,43 @@ end
 class Board
   include Enumerable
   #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
-
+attr_accessor :board
 
   def initialize
-    #TO DO :
+    @board = [BoardCases.new(),BoardCases.new(),BoardCases.new(),BoardCases.new(),BoardCases.new(),BoardCases.new(),BoardCases.new(),BoardCases.new()]
     #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
     #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
   end
 
   def to_s
-  #TO DO : afficher le plateau
+    @board.to_s
 
   end
 
   def play
-    #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+    @player1_plays.each do |x| @board[x].value = "X"
   end
+    @player2_plays.each do |x| @board[x].value = "O"
+  end
+end
 
   def victory?
-    #TO DO : qui gagne ?
+    win_comb = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    if (win_comb.include?(@player1_plays.sort))
+     'Victoire de #{@player1}'
+   elsif (win_comb.include?(@player2_plays.sort)
+    'Victoire de #{@player2}'
+  else
+    nil
   end
-
-end
 
 class Player
   #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
 
-  
+
   def initialize
     #TO DO : doit régler son nom, sa valeur, son état de victoire
   end
-
 end
 
 class Game
@@ -61,7 +67,7 @@ class Game
   def turn
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
   end
-
+end
 end
 
 Game.new.go
